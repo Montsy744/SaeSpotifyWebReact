34ab65c536a26e7830d3a8d2d44daf8ab8af7707
Hugo Straseele <straseelehugo@gmail.com>
2025-10-02 17:27:19 +0200

    update de la page d'acceuil maix il faut fix l'animation

--- src/page/Accueil.jsx
+++ src/page/Accueil.jsx
@@ -3,16 +3,37 @@ import { Link } from "react-router-dom";
 
 import HeaderSpotify from '../components/HeaderSpotify.jsx';
 
+import '../css/Accueil.css';
+
 function Accueil() {
     return (
         <Fragment>
-
             <HeaderSpotify />
-        <h1>Page d’accueil</h1>
-        <p>Bienvenue sur l’accueil de mon site React.</p>
+            <main>
+                <div id="mainContainer" className="parent">
+                    <div class="div1">1</div>
+                    <div class="div2">2</div>
+                    <div class="div3">3</div>
+                    <div class="div4">4</div>
+                    <div class="div5">
+                        <h1>Ils le disent mieux que nous ...</h1>
+                        <div class="carousel">
+                            <div class="group">
+                                <div class="card">A</div>
+                                <div class="card">B</div>
+                                <div class="card">C</div>
+                            </div>
 
-        {/* Lien vers la page Contact */}
-        <Link to="/">Aller à la page Home</Link>
+                            {/*Ajoutez `aria-hidden` pour masquer les cartes dupliquées aux lecteurs d'écran. */}
+                            <div aria-hidden class="group">
+                                <div class="card">A</div>
+                                <div class="card">B</div>
+                                <div class="card">C</div>
+                            </div>
+                            </div>
+                    </div>
+                </div>
+            </main>
         </Fragment>
     );
 }
