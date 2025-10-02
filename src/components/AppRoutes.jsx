e38ec6ceb8a97182a3d14272272de04f49c735a8
Hugo Straseele <straseelehugo@gmail.com>
2025-10-02 16:00:53 +0200

    ajout des routes vers les pages des velos voiture et covoiturage

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Accueil from "../page/Accueil.jsx";
 import Home from "../page/Home.jsx";
+import Voiture from "../page/voiture.jsx";
+import Velo from "../page/velo.jsx";
+import Covoiturage from "../page/covoiturage.jsx";
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/accueil" element={<Accueil />} />
+            <Route path="/voitures" element={<Voiture />} />
+            <Route path="/vélos" element={<Velo />} />
+            <Route path="/covoiturage" element={<Covoiturage />} />
         </Routes>
     );
 }
