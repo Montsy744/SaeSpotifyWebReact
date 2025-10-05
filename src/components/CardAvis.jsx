20b992adbb1ef0b50e05873e207b391710826d91
Hugo Straseele <straseelehugo@gmail.com>
2025-10-05 18:28:44 +0200

    ajout d'une font pour la div 5

--- src/components/CardAvis.jsx
+++ src/components/CardAvis.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-
+import './../css/CardAvis.css';
 
 function CardAvis(props) {
     return (
         <div class="card">
-            <h2>{props.nom}</h2>
-            <h3>{props.grade}</h3>
+            <div>
+                <h2>{props.nom} :</h2>
+                <h3>{props.grade}</h3>
+            </div>
             <p>{props.texte}</p>
         </div>
     );
