c98bb0b4b061181b42a4e1fb6060c36be9e7a553
Hugo Straseele <straseelehugo@gmail.com>
2025-12-05 21:17:49 +0100

    update de voiture.jsx

--- src/page/voiture.jsx
+++ src/page/voiture.jsx
@@ -1,16 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
-import HeaderSpotify from '../components/HeaderSpotify.jsx';
+import HeaderSpotify from "../components/HeaderSpotify.jsx";
+import BlocInfo from "../components/BlocInfo.jsx";
 
 function Voiture() {
-    return (
-        <Fragment>
-            <HeaderSpotify />
+  return (
+    <Fragment>
+        <HeaderSpotify />
 
-            
-        </Fragment>
-    );
+        <main>
+            <BlocInfo titre="Prêt de voitures">
+                <div>
+                    <p>
+                    Pour les salariés qui n'habiterait pas dans les villes où sont
+                    implanter nos bureau, pas de panique, nous avons aussi pensé a
+                    vous. En effet l'entreprise va maintenant pouvoir fournir, a ses
+                    salarié ne possédant pas de voiture ou possédant des voiture
+                    thermique, des voitures électriques.
+                    </p>
+                </div>
+            </BlocInfo>
+        </main>
+    </Fragment>
+  );
 }
 
 export default Voiture;
